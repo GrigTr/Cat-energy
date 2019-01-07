b5399ce3ae5f5ed5821683cd909bbd099f38426a
Grigoriy <grigtr7@gmail.com>
2019-01-07 03:22:51 +0300

    INdex-mobile version is done

---
+++ source/js/map.js
@@ -0,0 +1,56 @@
+
+ymaps.ready(init);
+var myMap;
+
+function init() {
+  if (document.body.clientWidth < 768) {
+    myMap = new ymaps.Map("map", {
+      center: [59.938631, 30.323055],
+      zoom: 17
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+
+    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map-pin.png',
+      iconImageSize: [72, 53],
+      iconImageOffset: [-36, -53]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  } else if (document.body.clientWidth >= 768 && document.body.clientWidth < 1300) {
+    myMap = new ymaps.Map("map", {
+      center: [59.938631, 30.323055],
+      zoom: 17
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+
+    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map-pin.png',
+      iconImageSize: [72, 53],
+      iconImageOffset: [-36, -53]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  } else if (document.body.clientWidth >= 1300) {
+    myMap = new ymaps.Map("map", {
+      center: [59.938963, 30.319293],
+      zoom: 17
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+
+    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/map-pin.png',
+      iconImageSize: [124, 106],
+      iconImageOffset: [-72, -106]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+  }
+}
+
